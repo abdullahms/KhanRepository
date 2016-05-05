78e240c413bebd7c4cd6a73d05a89eac4f50b073
abdullahms <abdullahkhan.sse@gmail.com>
2016-05-05 17:52:21 +0530

    Feature Demo (#1)
    * Added a comment
    
    * Added 2nd Comment

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            //this is my initial commit.
+            //Added another Comment.
         }
     }
 }
