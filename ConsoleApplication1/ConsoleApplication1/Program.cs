071e10ae53203ed4715062b6cf92edb6f94b2d9f
Abdullah Khan <abdullahkhan.sse@gmail.com>
2016-05-05 17:45:44 +0530

    Added 2nd Comment

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             //this is my initial commit.
+            //Added another Comment.
         }
     }
 }
